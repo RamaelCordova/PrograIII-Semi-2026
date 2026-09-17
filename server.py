3eeabc4fbbdb3712e90ac5f6ba6c61f124d9ab25
ramae <ramael.cordova14@gmail.com>
2026-09-16 23:44:23 -0600

    Sistema de impuestos a las actividades economicas

--- server.py
+++ server.py
@@ -1,22 +1,30 @@
-from http.server import SimpleHTTPRequestHandler
-server.py
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
 from urllib.parse import urlparse, parse_qs
 
 import json 
 
-PORT = 8080
+PORT = 3080
 
-class RequestHandler(BaseHTTPRequestHandler):
+class miservisor(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
+        url=urlparse(self.path)
+        qs = parse_qs(url.query)
+
+        if url.path == "/saludo":
+            saludo =qs["nombre"][0] + "biwnvenido a Phyton"\
+                
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(saludo.encode("utf-8"))
+
+        if url.path == '/':
             self.path = '/index.html'
         return SimpleHTTPRequestHandler.do_GET(self)
 
 print(f"Servidor corriendo en el puerto {PORT}")
-server = HTTPServer(('localhost', PORT), RequestHandler)
+server = HTTPServer(('localhost', PORT), miservisor)
 server.serve_forever()
             
             
